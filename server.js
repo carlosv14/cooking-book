e3bce169f47b53dfb19a4ebfd8b65e6485723940
Carlos Varela <cvarela1496@gmail.com>
2018-09-03 09:12:05 -0600

    more server configuration for deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/Cooking-Book'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/home.html'));
+res.sendFile(path.join(__dirname,'/dist/Cooking-Book/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
